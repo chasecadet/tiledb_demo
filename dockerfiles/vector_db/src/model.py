b132fe918eea15d724b2b9b8e1cee8d1cb6bea26
chase christensen <christensenc3526@gmail.com>
2024-02-03 23:04:11 +0000

    updated dockerfile and model

--- dockerfiles/vector_db/src/model.py
+++ dockerfiles/vector_db/src/model.py
@@ -20,12 +20,12 @@ EMBED_MODEL_NAME = os.getenv("EMBED_MODEL_NAME", "all-MiniLM-L6-v2")
 ACCESS_KEY = os.getenv("ACCESS_KEY", 'minio')
 SECRET_KEY= os.getenv("SECRET_KEY",'minio123')
 CLIENT_URL=os.getenv("CLIENT_URL","minio-service.kubeflow.svc.cluster.local:9000")
-BUCKET_NAME=os.getenv("BUCKET_NAME","newtiledb")
+
 class VectorStore(kserve.Model):
-    def __init__(self, name: str, bucket_name: str):
+    def __init__(self, name: str, docs_bucket_name: str):
         super().__init__(name)
         self.name = name
-        self._prepare_vectorstore(bucket_name)
+        self._prepare_vectorstore(docs_bucket_name)
         self.ready = True
 
     def _prepare_vectorstore(self, bucket_name: str):
@@ -35,7 +35,7 @@ class VectorStore(kserve.Model):
         except:
             print("index was not present. Creating a new index")
             client=get_client(ACCESS_KEY,SECRET_KEY,CLIENT_URL)
-            download_docs(BUCKET_NAME,client)
+            download_docs(bucket_name,client)
             docs = load_docs("tmp_docs")
             documents = process_docs(docs, chunk_size=500, chunk_overlap=0)
             self.vector_db = TileDB.from_documents(documents, embeddings, index_uri="tmp_index", index_type="FLAT")
@@ -52,7 +52,7 @@ class VectorStore(kserve.Model):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="VectorStore", description="VectorStore server")
     # Remove required=True for positional arguments
-    parser.add_argument("bucket_name", type=str, help="MinIO bucket location of the persisted VectorStore.")
+    parser.add_argument("docs_bucket_name", type=str, help="MinIO bucket location of the documents that need embedding.")
     args = parser.parse_args()
-    model = VectorStore("vectorstore", args.bucket_name)
+    model = VectorStore("vectorstore", args.docs_bucket_name)
     kserve.ModelServer(workers=1).start([model])
